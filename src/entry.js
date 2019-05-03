2dde3267e2213bb843a88de5841cfb7a1a09fe14
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-05-03 14:58:03 +0200

    Add support for browsers not handling shorthand properties in getComputedStyle

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,18 @@
+function getFontShorthand(element) {
+  const style = window.getComputedStyle(element);
+
+  return (
+    style.font
+    || `${style['font-style']} ${style['font-variant']} ${style['font-weight']} ${style['font-size']}/${style['line-height']} ${style['font-family']}`
+  );
+}
+
 export default function updateInputWidth(element) {
   if (typeof window === 'undefined') {
     return null;
   }
 
-  const { font } = window.getComputedStyle(element);
+  const font = getFontShorthand(element);
   const text = element.value || element.placeholder;
 
   const canvas = updateInputWidth.canvas || (updateInputWidth.canvas = document.createElement('canvas'));
