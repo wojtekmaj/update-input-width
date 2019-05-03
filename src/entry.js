f96525d4c1ce694e9c05c29afa8cb78b560e60a7
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-05-03 14:41:52 +0200

    Initial commit

---
+++ src/entry.js
@@ -0,0 +1,14 @@
+export default function updateInputWidth(element) {
+  const { font } = window.getComputedStyle(element);
+  const text = element.value || element.placeholder;
+
+  const canvas = updateInputWidth.canvas || (updateInputWidth.canvas = document.createElement('canvas'));
+  const context = canvas.getContext('2d');
+  context.font = font;
+  const { width } = context.measureText(text);
+  const ceilWidth = Math.ceil(width);
+
+  element.style.width = `${ceilWidth}px`;
+
+  return ceilWidth;
+}
